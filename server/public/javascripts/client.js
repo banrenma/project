b585f4c9671a59973f2b9c0692aa183dd4df0f8b
nieming <501182555@qq.com>
2015-09-22 23:48:49 +0800

    update

--- server/public/javascripts/client.js
+++ server/public/javascripts/client.js
@@ -29679,11 +29679,11 @@
 
 	var _routesComponentsIndexJsx2 = _interopRequireDefault(_routesComponentsIndexJsx);
 
-	var _routesComponentsUserJsx = __webpack_require__(164);
+	var _routesComponentsUserJsx = __webpack_require__(165);
 
 	var _routesComponentsUserJsx2 = _interopRequireDefault(_routesComponentsUserJsx);
 
-	var _componentsErrorJsx = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./components/error.jsx\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+	var _componentsErrorJsx = __webpack_require__(164);
 
 	var _componentsErrorJsx2 = _interopRequireDefault(_componentsErrorJsx);
 
@@ -31582,7 +31582,7 @@
 
 	function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-	var _componentsErrorJsx = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"../components/error.jsx\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+	var _componentsErrorJsx = __webpack_require__(164);
 
 	var _componentsErrorJsx2 = _interopRequireDefault(_componentsErrorJsx);
 
@@ -31623,6 +31623,58 @@
 
 /***/ },
 /* 164 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/* WEBPACK VAR INJECTION */(function(React) {'use strict';
+
+	Object.defineProperty(exports, '__esModule', {
+		value: true
+	});
+
+	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+	var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var _jsRoutesJs = __webpack_require__(159);
+
+	var _jsRoutesJs2 = _interopRequireDefault(_jsRoutesJs);
+
+	var Index = (function (_React$Component) {
+		_inherits(Index, _React$Component);
+
+		function Index() {
+			_classCallCheck(this, Index);
+
+			_get(Object.getPrototypeOf(Index.prototype), 'constructor', this).apply(this, arguments);
+		}
+
+		_createClass(Index, [{
+			key: 'handleclick',
+			value: function handleclick() {
+				_jsRoutesJs2['default'].gotoPath('/');
+			}
+		}, {
+			key: 'render',
+			value: function render() {
+				return React.createElement('div', null);
+			}
+		}]);
+
+		return Index;
+	})(React.Component);
+
+	exports['default'] = Index;
+	module.exports = exports['default'];
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
+
+/***/ },
+/* 165 */
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(React) {'use strict';
